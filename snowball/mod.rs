b356db97f648e2126de75e8fcc918c602f85ef39
NET-DIV <tmnabdou456@gmail.com>
2026-03-13 14:41:44 +0100

    Add files via upload

---
+++ snowball/mod.rs
@@ -0,0 +1,8 @@
+// add Snowball license in here
+pub mod algorithms;
+mod among;
+mod snowball_env;
+
+// why do we need this `crate::`?
+pub use crate::snowball::among::Among;
+pub use crate::snowball::snowball_env::SnowballEnv;
